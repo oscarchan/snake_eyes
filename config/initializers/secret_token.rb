f635427d18ec5ef398a79b08fd01f93ee4eb0eeb
Oscar Chan <oc.ideas@gmail.com>
2012-05-17 11:07:45 -0700

    skeleton project checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SnakeEyes::Application.config.secret_token = '9972a6f5874bcc09a8fcf1eb73ff332e4be8ec1e45e3a5b8701dbf256ad6fd14f522a474be6811877e84d3b22097f4a9e7266eaa5018c719a4cdb946b92e5166'
